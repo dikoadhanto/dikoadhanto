2bda36f6c151092c08eab421727dd7cf2428e67c
dikoadhanto <dickoadhato@gmail.com>
2022-08-11 15:15:47 +0700

    FilePHP

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Welcome!</title>
+
+    <!-- Poppins Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+    <!-- BoxIcons v2.1.2 -->
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <!-- CSS File -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+    <nav>
+        <div class="nav-brand">
+            <img src="https://cdn.dribbble.com/users/24078/screenshots/15522433/media/e92e58ec9d338a234945ae3d3ffd5be3.jpg?compress=1&resize=400x300">
+            <div class="info">
+                <h2>Hello!</h2>
+                <p>Hello World</p>
+            </div>
+        </div>
+        <ul class="nav-list-top">
+            <li class="nav-item">
+                <i class="bx bxs-dashboard"></i>
+                <span>Dashboard</span>
+            </li>
+            <li class="nav-item">
+                <i class="bx bx-bar-chart-alt-2"></i>
+                <span>Analytics</span>
+            </li>
+            <li class="nav-item">
+                <i class="bx bx-cart"></i>
+                <span>Orders</span>
+            </li>
+            <li class="nav-item">
+                <i class="bx bx-calendar"></i>
+                <span>Calendar</span>
+            </li>
+        </ul>
+        <ul class="nav-list-bottom">
+            <li class="nav-item">
+                <i class="bx bx-cog"></i>
+                <span>Settings</span>
+		</li>
+	 <li class="nav-item">
+                <i class="bx bx-star"></i>
+                <span>Favorites</span>
+            </li>
+            <li class="nav-item">
+                <i class="bx bx-phone"></i>
+                <span>Contact</span>
+            </li>
+        </ul>
+    </nav>
+
+</body>
+
+</html>
